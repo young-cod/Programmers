844b1ff8f672af409a8b07ce85386c73eccc9b0e
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-01-22 16:22:05 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 20.24 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,15 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        
-        Arrays.sort(num_list);
-        
-        int[] answer = new int[num_list.length - 5];
-        
-        for(int i = 0; i<num_list.length-5; i++){
-            answer[i] = num_list[i+5];
-        }
-        
-        return answer;
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
