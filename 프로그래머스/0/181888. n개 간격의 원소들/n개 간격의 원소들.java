3f1bad2275a3af653e861e819edcac2e11d5d528
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-02-07 11:01:37 +0900

    [level 0] Title: n개 간격의 원소들, Time: 12.82 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        return IntStream.range(0,num_list.length).filter(i->i%n==0).map(i->num_list[i]).toArray();
+    }
+}
