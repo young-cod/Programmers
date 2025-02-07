7ce983c48e8e47e90561c9f8624c3ba7c834e257
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-02-07 11:47:51 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,7 +1,7 @@
-import java.util.stream.IntStream;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = {};
-        return IntStream.range(n-1,num_list.length).map(i->num_list[i]).toArray();
+        return Arrays.copyOfRange(num_list,n-1,num_list.length);
     }
 }
