a49077e7e042314d7d17a13319a8669248232cda
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-02-10 15:33:07 +0900

    [level 0] Title: l로 만들기, Time: 23.88 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -2,6 +2,6 @@ import java.util.stream.Collectors;
 class Solution {
     public String solution(String myString) {
         String answer = "";
-        return myString.chars().mapToObj(c -> c < 'l' ? 'l' : (char)c).map(String::valueOf).collect(Collectors.joining());
+        return myString.replaceAll("[a-k]","l");
     }
 }
