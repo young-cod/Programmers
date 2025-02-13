e24add955b987c3f53f4bcea0fcd8a6038933bce
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-02-13 15:25:43 +0900

    [level 0] Title: 1로 만들기, Time: 1.95 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        answer = Arrays.stream(num_list).map(this::calcCount).sum();
+        return answer;
+    }
+    
+    //짝수라면 반으로 나누고
+    //홀수라면 1을 뺀 뒤 반으로 나누면
+     public int calcCount(int num) {
+        int count = 0;
+        
+        while (num != 1) {
+            if (num % 2 == 1) {
+                num = (num - 1) / 2;
+            } else {
+                num /= 2;
+            }
+            count++;
+        }
+        
+        return count;
+    }
+}
