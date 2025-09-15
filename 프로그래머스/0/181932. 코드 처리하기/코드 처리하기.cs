fce4595b21aabe4557b8e849f584566680a51ac0
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-09-15 16:25:37 +0900

    [level 0] Title: 코드 처리하기, Time: 900.28 ms, Memory: 49.9 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cs
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cs
@@ -2,34 +2,20 @@ using System;
 
 public class Solution {
     public string solution(string code) {
-          int mode = 0;
-  string answer = string.Empty;
+           bool mode = false;
+         string answer = string.Empty;
 
-  for (int i = 0; i < code.Length; i++)
-  {
-      if (mode == 0)
-      {
-          if (code[i] != '1')
-          {
-              if (i % 2 == 0) answer += code[i];
-          }
-          else if (code[i] == '1')
-          {
-              mode = 1;
-          }
-      }
-      else
-      {
-          if (code[i] != '1')
-          {
-              if (i % 2 == 1) answer += code[i];
-          }
-          else if (code[i] == '1')
-          {
-              mode = 0;
-          }
-      }
-  }
-  return answer == string.Empty ? "EMPTY" : answer;
+         for (int i = 0; i < code.Length; i++)
+         {
+             if (code[i] == '1')
+             {
+                 mode = !mode;
+                 continue;
+             }
+
+             if (mode && i % 2 == 1) answer += code[i];
+             else if(!mode && i%2==0)answer += code[i];
+         }
+         return answer == string.Empty ? "EMPTY": answer;
     }
 }
