4059af82ef3f5640f6baf5ec5b5fc4834435005c
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-09-23 17:58:50 +0900

    [level 1] Title: 명예의 전당 (1), Time: 8.31 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -4,29 +4,23 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int k, int[] score) {
-         int[] answer = new int[score.Length];
-        List<int> list = new List<int>();
+          int[] answer = new int[score.Length];
+         List<int> list = new List<int>();
 
-        list.Add(score[0]);
-        answer[0] = score[0];
-        for (int i = 1; i < score.Length; i++)
-        {
-            if (list.Count < k)
-            {
-                list.Add(score[i]);
-                list.Sort();
-            }
-            else
-            {
-                list.Add(score[i]);
-                list.Sort();
-                list.RemoveAt(0);
-            }
+         list.Add(score[0]);
+         answer[0] = score[0];
+         for (int i = 1; i < score.Length; i++)
+         {
 
-            answer[i] = list.Min();
-        }
+             list.Add(score[i]);
+             list.Sort();
+
+             if (list.Count > k) list.RemoveAt(0);
+
+             answer[i] = list.Min();
+         }
 
 
-        return answer;
+         return answer;
     }
 }
