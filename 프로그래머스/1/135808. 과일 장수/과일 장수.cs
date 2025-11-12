3748e74f29be0d6ed9507340cd26ccb7f605faaf
YoungSub <64890861+young-cod@users.noreply.github.com>
2025-11-12 15:37:28 +0900

    [level 1] Title: 과일 장수, Time: 162.59 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -1,27 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        List<int> list = score.ToList();
-        list.Sort();
-        list.Reverse();
-
-        List<int> check = new List<int>();
-        for (int i = 0; i < list.Count; i++)
-        {
-            if (check.Count < m)
-            {
-                check.Add(list[i]);
-            }
-
-            if (check.Count == m)
-            {
-                answer += check.Min() * m;
-                check.Clear();
-            }
+        Array.Sort(score);
+        
+        for(int i = score.Length-m; i>=0; i -= m){
+            answer += score[i] * m;
         }
 
         return answer;
